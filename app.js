535f9511c66fc8b89ba78dbe2864fec970c321fd
Aravindhan V <varavindhan2010@gmail.com>
2019-10-01 15:04:49 +0530

    Final Backend

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ var Judge = require('./models/judge');
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/judging");
 
+var round=1;
+
 // Team.create(
 //     {
-//         team_id : "#4",
-//         name: "C Sick",
-//         room: "Room 004",
+//         team_id : "#1",
+//         name: "cd 8086",
+//         room: "CAD Lab",
 //         scores: [
 //             [
-//                 {
-//                     judge_id: "judge002",
-//                     score: -1
-//                 }
+//                 // {
+//                 //     judge_id: "bE6AqAXccXgZQIN8NK5i1rrxTJY2",
+//                 //     score: -1
+//                 // }
 //             ]
 //         ]
 //     }
@@ -27,67 +29,34 @@ mongoose.connect("mongodb://localhost/judging");
 
 // Judge.create(
 //     {
-//         judge_id: "judge001",
+//         judge_id: "diHiyjVlCBdBdsnB6REcepRESvI3",
 //         round: 1,
 //         teams: [
-//             "#3","#4"
+//             "#1","#5"
 //         ]
 //     }
 // );
 
 app.get('/teams/:judgeid',function(req,res){
-    // var res1 = {
-    //     index: "#3",
-    //     name: "Test Team",
-    //     room: "Seminar Room"
-    // }
-    // var res2 = {
-    //     index: "#4",
-    //     name: "Test Team 1",
-    //     room: "Seminar Room 111"
-    // }
-    // Team.find({},function(err,team){
-    //     if(err){
-    //         console.log(err)
-    //     }
-    //     else{
-    //         // console.log("Hello: "+team);
-    //         res.send(team);
-    //     }
-    // });
-    Judge.find({judge_id:""+req.params.judgeid},function(err,dbjudge){
+    Judge.find({judge_id: req.params.judgeid},function(err,dbjudge){
         if(err){
             console.log(err);
         }
         else{
             var jteams = dbjudge[0].teams;
+            // console.log(jteams);
             Team.find({'team_id': {$in : jteams}},function(err,dbteams){
                 if(err){
                     console.log(err);
                 }
                 else{
-                    res.send(dbteams);
+                    dbteams.sort(function(a,b){
+                        return a.team_id.localeCompare(b.team_id);
+                    });
+                    // console.log({round: round, teams: dbteams});
+                    res.send({round: round, teams: dbteams});
                 }
             });
-            // lteams=[];
-            // jteams.forEach(async function(team_id){
-            //     console.log(team_id);
-            //     await Team.find({team_id: team_id},function(err,dbteams){
-            //         if(err){
-            //             console.log(err);
-            //         }
-            //         else{
-            //             console.log("IN");
-            //             lteams.push(dbteams[0]);
-                        
-            //         }
-            //     });
-            //     // console.log(lteams);
-            //     console.log("OUT");
-            // });
-            // console.log("ForEach done");
-            // console.log(lteams);
-            // res.send(lteams);
         }
     });
 });
@@ -95,28 +64,38 @@ app.get('/teams/:judgeid',function(req,res){
 app.post('/scores',function(req,res){
     var score = req.body.score;
     var uid = req.body.jid;
-    console.log(uid);
+    var tid = req.body.tid;
+    console.log(score);
     Judge.findOne({judge_id: uid},function(err,dbjudge){
         if(err){
             console.log(err);
         }
         else{
             var teamstobejudged = dbjudge.teams;
-            var idx = teamstobejudged.indexOf("#3");
-            console.log(dbjudge);
+            var idx = teamstobejudged.indexOf(tid);
             teamstobejudged.splice(idx,1);
-            console.log(teamstobejudged);
-            Judge.findByIdAndUpdate(dbjudge._id,{'teams': teamstobejudged},function(err,updatedJudge){
+            dbjudge.teams = teamstobejudged;
+            dbjudge.save(function(err,updatedJudge){
                 if(err){
                     console.log(err);
                 }
                 else{
-                    console.log(updatedJudge);
+                    Team.findOne({team_id: tid},function(err,dbteam){
+                        if(err){
+                            console.log(err);
+                        }
+                        else{
+                            dbteam.scores[dbjudge.round-1].push({judge_id: dbjudge.judge_id,score: score});
+                            dbteam.save(function(err,updatedTeam){
+                                res.redirect('/teams/'+uid);
+                            });
+                        }
+                    });
+                    // res.redirect('/teams/'+uid);
                 }
             });
         }
     });
-    res.redirect('/teams');
 });
 
 app.listen(8080,function(){
